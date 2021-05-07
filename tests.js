aa2e2d98555fcc1fa654ecfc50742b64487d1f56
Alejandro Ugarte Ruiz <43729610+alexaur97@users.noreply.github.com>
2021-05-07 08:37:14 +0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,3 +1,11 @@
 import {sampleTest} from 'tests/sampleTest.js';
 
-sampleTest();
+describe('Sample test', () => {
+  it('object type', () => {
+    expect(typeof 2).toBe('number');
+  });
+
+  it('Assert equals', () => {
+    expect(2).toEqual(2);
+  });
+});
