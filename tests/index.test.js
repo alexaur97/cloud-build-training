b91f3f07b46c8ec536b163ec7b39332954bef94f
alexaur97 <ugarte97aur@gmail.com>
2021-05-07 09:22:55 +0200

    changed main from test to index

---
+++ tests/index.test.js
@@ -0,0 +1,10 @@
+
+describe('Sample test', () => {
+    it('object type', () => {
+    expect(typeof 2).toBe('number');
+    });
+
+    it('Assert equals', () => {
+        expect(2).toEqual(2);
+    });
+});
